3e1e60412a35d32f3951f0f17ddb28f0b2f913f3
kADURICO <caduribs@gmail.com>
2026-02-10 21:44:09 -0300

    adicionado crud simples usando express

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
 const testJson = {
     "times": [
         {
@@ -27,12 +29,40 @@ const testJson = {
     ]
 }
 
+const findTime = (id) => {
+    return testJson.times.find((time) => time.id == id);
+}
+
+const findTimeIndex = (id) => {
+    return testJson.times.findIndex((time) => time.id == id);
+}
+
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
 app.get("/times", (req, res) => {
-    res.send(testJson);
+    res.status(200).send(testJson);
+});
+
+app.get("/times/:id", (req, res) => {
+    res.json(findTime(req.params.id));
+});
+
+app.post("/times", (req, res) => {
+    testJson.times.push(req.body);
+    res.status(201).send('Time adicionado com sucesso!');
+});
+
+app.put("/times/:id", (req, res) => {
+    const timeIndex = findTimeIndex(req.params.id);
+    testJson.times[timeIndex] = req.body;
+    res.status(200).send('Time atualizado com sucesso!');
+});
+
+app.delete("/times/:id", (req, res) => {
+    testJson.times.splice(findTimeIndex(req.params.id), 1);
+    res.status(200).send('Time deletado com sucesso!');
 });
 
 export default app;
