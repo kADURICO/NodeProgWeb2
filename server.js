2142b0434e22e3a692ace02b4f7125df0fb363d8
kADURICO <caduribs@gmail.com>
2026-02-24 21:31:16 -0300

    adiciona mais times, conecta com mysql e adiciona nova arquitetura

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 import app from "./src/app.js";
+import conexao from "./infra/conexao.js";
 
 const port = 4200;
 
-app.listen(port, () => {
-    console.log(`Server rodando http://localhost:${port}`);
+conexao.connect((erro) => {
+    if (erro) {
+        console.log("Erro: " + erro);
+    } else {
+        console.log("Conectado ao banco de dados");
+
+        app.listen(port, () => {
+            console.log(`Server rodando http://localhost:${port}`);
+        });
+    }
 });
+
--- src/app.js
+++ src/app.js
@@ -8,22 +8,102 @@ const testJson = {
     "times": [
         {
             "id": 1,
+            "nome": "América-MG",
+            "cidade": "Belo Horizonte"
+        },
+        {
+            "id": 2,
+            "nome": "Athletico-PR",
+            "cidade": "Curitiba"
+        },
+        {
+            "id": 3,
+            "nome": "Atlético-MG",
+            "cidade": "Belo Horizonte"
+        },
+        {
+            "id": 4,
+            "nome": "Bahia",
+            "cidade": "Salvador"
+        },
+        {
+            "id": 5,
+            "nome": "Botafogo",
+            "cidade": "Rio de Janeiro"
+        },
+        {
+            "id": 6,
+            "nome": "Red Bull Bragantino",
+            "cidade": "Bragança Paulista"
+        },
+        {
+            "id": 7,
+            "nome": "Chapecoense",
+            "cidade": "Chapecó"
+        },
+        {
+            "id": 8,
+            "nome": "Corinthians",
+            "cidade": "São Paulo"
+        },
+        {
+            "id": 9,
+            "nome": "Coritiba",
+            "cidade": "Curitiba"
+        },
+        {
+            "id": 10,
+            "nome": "Cruzeiro",
+            "cidade": "Belo Horizonte"
+        },
+        {
+            "id": 11,
             "nome": "Flamengo",
             "cidade": "Rio de Janeiro"
         },
         {
-            "id": 2,
-            "nome": "Vasco",
-            "cidade": "Rio de Janeiro"
-        },
-        {
-            "id": 3,
+            "id": 12,
             "nome": "Fluminense",
             "cidade": "Rio de Janeiro"
         },
         {
-            "id": 4,
-            "nome": "Botafogo",
+            "id": 13,
+            "nome": "Grêmio",
+            "cidade": "Porto Alegre"
+        },
+        {
+            "id": 14,
+            "nome": "Internacional",
+            "cidade": "Porto Alegre"
+        },
+        {
+            "id": 15,
+            "nome": "Mirassol",
+            "cidade": "Mirassol"
+        },
+        {
+            "id": 16,
+            "nome": "Palmeiras",
+            "cidade": "São Paulo"
+        },
+        {
+            "id": 17,
+            "nome": "Remo",
+            "cidade": "Belém"
+        },
+        {
+            "id": 18,
+            "nome": "Santos",
+            "cidade": "Santos"
+        },
+        {
+            "id": 19,
+            "nome": "São Paulo",
+            "cidade": "São Paulo"
+        },
+        {
+            "id": 20,
+            "nome": "Vasco",
             "cidade": "Rio de Janeiro"
         }
     ]
