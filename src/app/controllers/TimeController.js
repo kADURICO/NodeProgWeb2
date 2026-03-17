19d0127fc3c3109befcbf68aa52d79c7c98de960
kADURICO <caduribs@gmail.com>
2026-03-17 02:22:30 -0300

    adiciona a arquitetura de promises no repository e controller

--- src/app/controllers/TimeController.js
+++ src/app/controllers/TimeController.js
@@ -2,28 +2,75 @@ import TimeRepository from "../repositories/TimeRepository.js";
 
 class TimeController {
 
-    index(req, res) {
-        TimeRepository.index(req, res);
+    async index(req, res) {
+        try {
+            const result = await TimeRepository.index();
+            res.status(200).json(result);
+        } catch (err) {
+            console.error("Erro:", err);
+            res.status(500).json({ erro: err });
+        }
     }
 
-    show(req, res) {
-        TimeRepository.show(req, res);
+    async show(req, res) {
+        try {
+            const id = req.params.id;
+            const result = await TimeRepository.show(id);
+            res.status(200).json(result);
+        } catch (err) {
+            console.error("Erro:", err);
+            res.status(500).json({ erro: err });
+        }
     }
 
-    store(req, res) {
-        TimeRepository.store(req, res);
+    async store(req, res) {
+        try {
+            const dados = req.body;
+            const result = await TimeRepository.store(dados);
+            res.status(201).json({ mensagem: result });
+        } catch (err) {
+            console.error("Erro:", err);
+            res.status(500).json({ erro: err });
+        }
     }
 
-    update(req, res) {
-        TimeRepository.update(req, res);
+    async update(req, res) {
+        try {
+            const dados = req.body;
+            const id = req.params.id;
+            const result = await TimeRepository.update(dados, id);
+            res.status(200).json({ mensagem: result });
+        } catch (err) {
+            console.error("Erro:", err);
+            res.status(500).json({ erro: err });
+        }
     }
 
-    delete(req, res) {
-        TimeRepository.delete(req, res);
+    async delete(req, res) {
+        try {
+            const id = req.params.id;
+            const result = await TimeRepository.delete(id);
+            res.status(200).json({ mensagem: result });
+        } catch (err) {
+            console.error("Erro:", err);
+            res.status(500).json({ erro: err });
+        }
     }
 
-    searchByName(req, res) {
-        TimeRepository.searchByName(req, res);
+    async searchByName(req, res) {
+        try {
+            const { nome } = req.query;
+
+            if (!nome) {
+                return res.status(400).json({ erro: "Parâmetro 'nome' é obrigatório." });
+            }
+
+            const result = await TimeRepository.searchByName(nome);
+            res.status(200).json(result);
+        } catch (err) {
+            console.error("Erro:", err);
+            res.status(500).json({ erro: err });
+        }
     }
 }
 
--- src/app/repositories/TimeRepository.js
+++ src/app/repositories/TimeRepository.js
@@ -1,76 +1,63 @@
 import conexao from "../database/index.js";
 
-
 class TimeRepository {
-    index(req, res) {
+    index() {
         const sql = "SELECT * FROM db_brasileirao.campeonato";
-        conexao.query(sql, (err, result) => {
-            if (err) {
-                console.error("Erro:", err);
-                return res.status(500).json({ erro: err });
-            }
-            res.status(200).json(result);
+        return new Promise((resolve, reject) => {
+            conexao.query(sql, (err, result) => {
+                if (err) return reject(err);
+                return resolve(result);
+            });
         });
     }
 
-    searchByName(req, res) {
-        const { nome } = req.query;
-
-        if (!nome) {
-            return res.status(400).json({ erro: "Parâmetro 'nome' é obrigatório." });
-        }
-
+    searchByName(nome) {
         const sql = "SELECT * FROM db_brasileirao.campeonato WHERE nome LIKE ?";
-        conexao.query(sql, [`%${nome}%`], (err, result) => {
-            if (err) {
-                console.error("Erro:", err);
-                return res.status(500).json({ erro: err });
-            }
-            res.status(200).json(result);
+        return new Promise((resolve, reject) => {
+            conexao.query(sql, [`%${nome}%`], (err, result) => {
+                if (err) return reject(err);
+                return resolve(result);
+            });
         });
     }
 
-    show(req, res) {
+    show(id) {
         const sql = "SELECT * FROM db_brasileirao.campeonato WHERE id = ?";
-        conexao.query(sql, req.params.id, (err, result) => {
-            if (err) {
-                console.error("Erro:", err);
-                res.status(500).json({ erro: err });
-            }
-            res.status(200).json(result);
+        return new Promise((resolve, reject) => {
+            conexao.query(sql, id, (err, result) => {
+                if (err) return reject(err);
+                return resolve(result);
+            });
         });
     }
 
-    store(req, res) {
-        const sql = "INSERT INTO db_brasileirao.campeonato SET ?"
-        conexao.query(sql, req.body, (err, result) => {
-            if (err) {
-                console.error("Erro:", err);
-                res.status(500).json({ erro: err });
-            }
-            res.status(201).json({ mensagem: result });
+    store(dados) {
+        const sql = "INSERT INTO db_brasileirao.campeonato SET ?";
+        return new Promise((resolve, reject) => {
+            conexao.query(sql, dados, (err, result) => {
+                if (err) return reject(err);
+                return resolve(result);
+            });
         });
     }
 
-    update(req, res) {
-        const sql = "UPDATE db_brasileirao.campeonato SET ? WHERE id = ?"
-        conexao.query(sql, [req.body, req.params.id], (err, result) => {
-            if (err) {
-                console.error("Erro:", err);
-                res.status(500).json({ erro: err });
-            }
-            res.status(200).json({ mensagem: result });
+    update(dados, id) {
+        const sql = "UPDATE db_brasileirao.campeonato SET ? WHERE id = ?";
+        return new Promise((resolve, reject) => {
+            conexao.query(sql, [dados, id], (err, result) => {
+                if (err) return reject(err);
+                return resolve(result);
+            });
         });
     }
 
-    delete(req, res) {
-        const sql = "DELETE FROM db_brasileirao.campeonato WHERE id = ?"
-        conexao.query(sql, req.params.id, (err, result) => {
-            if (err) {
-                console.error("Erro:", err);
-                res.status(500).json({ erro: err });
-            }
-            res.status(200).json({ mensagem: result });
+    delete(id) {
+        const sql = "DELETE FROM db_brasileirao.campeonato WHERE id = ?";
+        return new Promise((resolve, reject) => {
+            conexao.query(sql, id, (err, result) => {
+                if (err) return reject(err);
+                return resolve(result);
+            });
         });
     }
 }
